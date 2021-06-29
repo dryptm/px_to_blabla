b91ec4bbf9787b126a71fb7def6d3121ed3dd64d
Vinaya Kumar <vinay9415756155@gmail.com>
2021-06-29 21:20:53 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,18 @@
+var a="" //put your string in here
+var  b=a.split("px")
+var c=[]
+for(var i=0;i<b.length-1;i++){
+var w=""
+for(var j=(b[i].length-1);j>=0;j--){
+if(b[i][j]==" " || b[i][j]==":"){
+    if(b[i][j]===":") b[i] = b[i].substring(0,j)+":"+String(Number(w)*(100/1536))+"vw"
+    else if(b[i][j]===" ") b[i] = b[i].substring(0,j)+" "+String(Number(w)*(100/1536))+"vw"
+    
+break;    
+}
+else{
+w=b[i][j]+w;
+}
+}}
+
+var ans = b.join('')
